3a8f0beae2d3f13fea1d2b91c6d12ae4be821ace
rafay183 <rafayshaikh633@gmail.com>
2024-11-06 16:29:13 +0500

    this a test file

---
+++ 123.py
@@ -0,0 +1,23 @@
+
+
+a=10
+b=15
+if a<=b and a<b and b>a:
+    print("A is less than b")
+elif b>a and a!=b and b!=a:
+    print("b is greater than a")   
+else:
+    print("Sort out your issues")
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ python2.py
@@ -0,0 +1,27 @@
+
+'''
+arithmetic operators:
+    +
+    -
+    /
+    *
+    %
+    
+    '''
+
+'''print("hello world")
+input("Enter:")'''
+'''
+import keyword
+print(keyword.kwlist)
+'''
+'''
+num1=eval(input("Enter your 1st value:"))
+num2=eval(input("Enter your 2nd value:"))
+print('This is Addition:',num1+num2,"\nThis is Multiplication:",num1*num2,"\nThis is division:",num1/num2,"\nThis is Subtraction:",num1-num2)
+'''
+
+
+
+e,i,p=eval,input,print;num1,num2=e(i("Enter your first value:")),e(i("Enter your second value:"));p('This is Addition:',num1+num2,"\nThis is Multiplication:",num1*num2,"\nThis is division:",num1/num2,"\nThis is Subtraction:",num1-num2)
+
